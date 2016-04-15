561471b0b80e2332fba56e914181736a81168ac4
Vince Bloise <vbloise3@verizon.net>
2016-04-15 19:44:37 -0400

    Added more middleware

--- app.js
+++ app.js
@@ -2,19 +2,34 @@
  * Created by vincebloise on 4/14/16.
  */
 var express = require("express");
+var logger = require("morgan");
+var path = require("path");
 var http = require("http");
 
 var app = express();
+
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
 app.use(function (request, response, next) {
     console.log("In comes a request to: ", request.url);
     next();
 });
 
+app.use(function(request, response, next) {
+    var minute = (new Date()).getMinutes();
+    if ((minute % 2) === 0) {
+        next();
+    } else {
+        response.statusCode = 403;
+        response.end("Not authorized");
+    }
+});
+
 app.use(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello looser!");
-})
+    response.end('Secret info: the password is "PulpFiction"!');
+});
 
 http.createServer(app).listen(3001);
 console.log("listening on port 3001");
