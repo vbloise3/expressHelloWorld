250e77ca852907de2276789bd96b1ccf10a9083b
Vince Bloise <vbloise3@verizon.net>
2016-04-18 20:13:10 -0400

    Added route middleware

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ var app = express();
 var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
+app.get("/", function(request, response) {
+    response.end("Welcome to my homepage");
+});
+
+app.get("/about", function(request, response) {
+    response.end("Welcome to the about page");
+});
+
+app.get("/weather", function(request, response) {
+    response.end("The current weather is NICE");
+});
+
+app.get("/hello/:who", function(request, response) {
+    response.end("Hello, " + request.params.who + ".");
+});
+
+app.use(function(request, response) {
+    response.statusCode = 404;
+    response.end("404");
+});
+
 app.use(function (request, response, next) {
     console.log("In comes a request to: ", request.url);
     next();
